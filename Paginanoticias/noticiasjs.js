588ea4dd6501c807817542a0e4e3274905761b68
Santiago Delgado <santiago.delgado.icc@gmail.com>
2025-06-14 17:42:30 -0400

    avance pagina noticias y favoritos

---
+++ Paginanoticias/noticiasjs.js
@@ -0,0 +1,28 @@
+const apiKey = 'efaaf1c4-11e3-46d2-b756-584da68ed133';
+const noticiasContainer = document.getElementById('noticias');
+
+fetch(`https://content.guardianapis.com/search?q=books&section=books&api-key=${apiKey}&show-fields=thumbnail,trailText,byline`)
+  .then(res => res.json())
+  .then(data => {
+    const noticias = data.response.results;
+    noticias.forEach(noticia => {
+      const div = document.createElement('div');
+      div.classList.add('col-md-4', 'mb-4');
+
+      div.innerHTML = `
+        <div class="card h-100">
+          ${noticia.fields.thumbnail ? `<img src="${noticia.fields.thumbnail}" class="card-img-top" alt="Imagen">` : ''}
+          <div class="card-body">
+            <h5 class="card-title">${noticia.webTitle}</h5>
+            <p class="card-text">${noticia.fields.trailText || ''}</p>
+            <a href="${noticia.webUrl}" target="_blank" class="btn btn-outline-light">Leer más</a>
+          </div>
+        </div>
+      `;
+      noticiasContainer.appendChild(div);
+    });
+  })
+  .catch(err => {
+    noticiasContainer.innerHTML = '<p class="text-danger">Error al cargar noticias.</p>';
+    console.error(err);
+  });
