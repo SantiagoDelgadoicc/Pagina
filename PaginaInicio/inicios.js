b18d279aad06203774e0631dabad2968df3c1788
Bastian Hernandez <bastian.hernandez.23@alumnos.uda.cl>
2025-06-14 20:26:18 -0400

    CON EL INICIO FUNCIONANDO BIEN

--- PaginaInicio/inicios.js
+++ PaginaInicio/inicios.js
@@ -1,26 +1,36 @@
-const contenedor= document.getElementById("contenedor_libros");
+const contenedor = document.getElementById("contenedor_libros");
 
-fetch("")
-    .then(response => {
-      if (!response.ok) throw new Error("No se pudo obtener la ubicación");
-      return response.json();
-    })
-    .then(data => {
-        const libros = data.libros; 
-        libros.forEach(libro => {
-            const informacion =
-            const titulo=
-            const imagen =
+fetch("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=Yg838GVGDPxGsV0EpVbAmKXRwzHNWDzB")
+  .then(response => {
+    if (!response.ok) throw new Error("No se pudo obtener la lista de libros");
+    return response.json();
+  })
+  .then(data => {
+    
+    const libros = data.results.books.slice(0, 6); // Limita a 6 libros
 
-            const card = document.createElement('div');
-                card.className = 'card';
-                card.innerHTML = `
-                    <img src="${imagen}" ">
-                    <h3>${titulo}</h3>
-                `;
-                contenedor.appendChild(card);
-                });
-            })
-    .catch(error => {
+    libros.forEach(libro => {
+      const titulo = libro.title || 'Sin título';
+      const imagen = libro.book_image || 'https://via.placeholder.com/100x150?text=No+Image';
+      const autor = libro.author || 'Autor desconocido';
+      const ranking_ventas = libro.rank || 'No hay descripción disponible';
+      const publisher = libro.publisher || 'Editorial desconocida';
+      const amazon_link = libro.amazon_product_url || '#';
+
+      const card = document.createElement('div');
+      card.className = 'card';
+      card.innerHTML = `
+        <img src="${imagen}" ">
+        <h3>${titulo}</h3>
+        <p><em>${autor}</em></p>
+        <p>${ranking_ventas}</p>
+        <p class="editorial_config_titulo"><strong>Editorial:</strong> </p>
+        <p class="editorial_config"> ${publisher}</p>
+        <a href="${amazon_link}" target="_blank" class="btn boton_libros_destacados">Comprar en Amazon</a>
+      `;
+      contenedor.appendChild(card);
+    });
+  })
+  .catch(error => {
     contenedor.innerHTML = `<p>Error al cargar los libros: ${error.message}</p>`;
   });
