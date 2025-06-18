34f1d97e604f34d30ba0bb05cdedf381a956fc9f
francisca paz <franpazleyton@gmail.com>
2025-06-18 16:06:26 -0400

    funcionando bien uso de api primera parte

--- Paginacatalogo/scripts.js
+++ Paginacatalogo/scripts.js
@@ -18,16 +18,36 @@ function añadirtarjeta(eleccion){
       return response.json();
     })
      .then(data => {
-        let n = Math.random() * data.items.length;
-        n = Math.trunc(n);
-        //items es una lista de objetos
         //ver la validacion por si descripcion e imagen no existe
-        let libro = data.items[n]
-        if (!ids.includes(libro.id)){
-            ids.push(libro.id);
-            const tarjeta = document.createElement('div');
-            tarjeta.className = 'card mx-3 p-0 mt-3 tarjeta';
-            tarjeta.innerHTML = `<div class="posicioncirculo">
+        let libro;
+        let encontrado = false;
+        let intentos = 0;
+        //si el libro no tiene ni imagen o desc que busque otro
+        while (!encontrado && intentos < 100) {
+                let n = Math.random() * data.items.length;
+                n = Math.trunc(n);
+                libro = data.items[n];
+                if (!ids.includes(libro.id)) {//evita que el libro se repita
+                    if (libro.volumeInfo.imageLinks.thumbnail && libro.volumeInfo.description) {
+                        encontrado = true;
+                        ids.push(libro.id);
+                    }
+                }
+                intentos++;
+        }
+        //por si no encuentra libro despues de 100 intentos
+        if (!encontrado) {
+            console.log("no se encontro libro");
+        }
+        //si la descripcion es muy larga rellena al conal con puntos
+        let descripcion = libro.volumeInfo.description;
+        if (descripcion.length > 100) {
+            descripcion = descripcion.substring(0, 100) + '...';
+        }
+        //creacion de la tarjeta
+        const tarjeta = document.createElement('div');
+        tarjeta.className = 'card mx-3 p-0 mt-3 tarjeta';
+        tarjeta.innerHTML = `<div class="posicioncirculo">
                                 <button class="circulo">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-heart-fill corazon" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
@@ -35,14 +55,13 @@ function añadirtarjeta(eleccion){
                                 </button>
                             </div>
 
-                            <img src=${libro.volumeInfo.imageLinks.thumbnail} class="card-img-top" height="200px" width="100%" alt="libro">
+                            <img src=${libro.volumeInfo.imageLinks.thumbnail} class="card-img-top" height="280px" width="100%" alt="libro">
                             <div class="card-body tarjetacuerpo">
-                                <!-- <h5 class="card-title">Card title</h5> -->
-                                <p class="card-text mt-2 letraoscura">${libro.volumeInfo.description.substring(0, 100)}</p>
-                                <a href="#" class="btn mt-2 botonoscuro">ver reseñas</a>
+                                <p class="card-text mt-2 letraoscura">${descripcion}</p>
+                                <a href="#" class="btn mt-2 botonoscuro id="botontarjeta">ver reseñas</a>
                             </div>`
-            secciontarjetas.appendChild(tarjeta);
-        }
+        secciontarjetas.appendChild(tarjeta);
+        
 
     })
      .catch(error => {
